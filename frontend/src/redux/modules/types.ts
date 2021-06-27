ae33e9e795e7585057c67f0d02258d2f16f9db12
anicioalexandre <acosta@phone2action.com>
2021-06-27 14:02:42 -0300

    feat: creates login redux module

---
+++ frontend/src/redux/modules/types.ts
@@ -0,0 +1,5 @@
+import { LoginState } from './login'
+
+export type GlobalState = {
+  login: LoginState
+}
---
+++ frontend/src/redux/reducer.ts
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux'
+import login from './modules/login'
+
+export default combineReducers({ login })
