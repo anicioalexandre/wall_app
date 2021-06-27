131e3a201bc032c906b3d4224ddc08425f33c605
anicioalexandre <acosta@phone2action.com>
2021-06-27 14:06:27 -0300

    feat: adds routes to the app

--- frontend/src/App/index.tsx
+++ frontend/src/App/index.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react'
+import Routes from '../Routes'
 
-const App: FC = () => <div className="text-success">Wall app</div>
-
+const App: FC = () => (
+  <div className="flex flex-col items-center h-screen">
+    <Routes />
+  </div>
+)
 export default App
