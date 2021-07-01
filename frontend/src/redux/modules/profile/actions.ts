7e210e75ef37be74fa7b0d45f5ef2003ffe3df71
anicioalexandre <acosta@phone2action.com>
2021-06-30 22:00:03 -0300

    refactor: refactors profile redux module

---
+++ frontend/src/redux/modules/profile/actions.ts
@@ -0,0 +1,66 @@
+import { Dispatch } from 'redux'
+
+import { DataType, FetchParams } from '../../../services/types'
+import fetchEndpoint from '../../../services/api'
+import {
+  transformProfileDataToBE,
+  transformProfileDataToFE,
+  transformProfileErrorToFE
+} from './transformers'
+import {
+  ProfileFailureAction,
+  ProfileRequestAction,
+  ProfileSuccessAction
+} from './types'
+
+export const REQUEST_PROFILE = 'REQUEST_PROFILE'
+export const REQUEST_PROFILE_SUCCESS = 'REQUEST_PROFILE_SUCCESS'
+export const REQUEST_PROFILE_FAILURE = 'REQUEST_PROFILE_FAILURE'
+
+const requestProfile = (): ProfileRequestAction => ({
+  type: REQUEST_PROFILE
+})
+
+const requestProfileSuccess = ({
+  profile
+}: Omit<ProfileSuccessAction, 'type'>): ProfileSuccessAction => {
+  return {
+    type: REQUEST_PROFILE_SUCCESS,
+    profile
+  }
+}
+
+const requestProfileFailure = ({
+  error
+}: Omit<ProfileFailureAction, 'type'>): ProfileFailureAction => {
+  return {
+    type: REQUEST_PROFILE_FAILURE,
+    error
+  }
+}
+
+export const profileApi = ({ method, endpoint, data }: FetchParams) => {
+  const transformedData = transformProfileDataToBE(data)
+  return (
+    dispatch: Dispatch
+  ): Promise<ProfileSuccessAction | ProfileFailureAction> => {
+    dispatch(requestProfile())
+    return fetchEndpoint({
+      method,
+      endpoint,
+      data: transformedData
+    }).then(
+      (profile: DataType) => {
+        const profileTransformed = transformProfileDataToFE(profile)
+
+        return dispatch(requestProfileSuccess({ profile: profileTransformed }))
+      },
+      (error) => {
+        const errorTransformed = transformProfileErrorToFE(error)
+        console.log(errorTransformed, 'errorTransformed')
+        console.log(error, 'error')
+        return dispatch(requestProfileFailure({ error: errorTransformed }))
+      }
+    )
+  }
+}
