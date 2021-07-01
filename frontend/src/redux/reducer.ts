aa167e5898d3b31602d49fb3455e5690acae3042
anicioalexandre <acosta@phone2action.com>
2021-06-30 21:59:20 -0300

    refactor: refactors auth redux module

--- frontend/src/redux/reducer.ts
+++ frontend/src/redux/reducer.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 
-import auth from './modules/auth'
-import profile from './modules/profile'
+import auth from './modules/auth/reducer'
+import profile from './modules/profile/reducer'
 
 export default combineReducers({ auth, profile })
