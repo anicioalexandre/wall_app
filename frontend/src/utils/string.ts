ec81ade8f0238f8076cd24418d3fdc3b4634490a
anicioalexandre <acosta@phone2action.com>
2021-07-04 02:47:25 -0300

    chore: creates string util function

--- frontend/src/utils/string.ts
+++ frontend/src/utils/string.ts
@@ -1,3 +1,16 @@
 export const capitalizeFirstLetter = (text: string): string => {
   return text?.charAt(0).toUpperCase() + text?.slice(1) || ''
 }
+
+const filterEmptyValues = (list: unknown[]) => {
+  return list?.filter((item) => item) || []
+}
+
+export const joinWithSeparator = (
+  list: unknown[],
+  separator = ', '
+): string => {
+  const filteredList = filterEmptyValues(list)
+
+  return filteredList.join(separator)
+}
